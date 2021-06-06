2bb3106e32d0abb440459cd1a58b387f28aa5233
HaneenHaddad99 <>
2021-06-06 11:50:28 +0000

    makepage

---
+++ script.js
@@ -0,0 +1,17 @@
+
+
+
+var connect="home";
+var emailConnect="email";
+var cntactus="phone number";
+console.log(connect);
+
+
+alert("best for making flower arrangement"+connect)
+
+document.write("<p style='color:green;'></p>")
+
+var numberofFlowerYouNeed=prompt("enter the number of flower you need");
+var flowerNumber=50
+if(numberofFlowerYouNeed<=50) {alert ("git the discount")}else{alert("hello")}
+
